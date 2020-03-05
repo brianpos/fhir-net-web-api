f209f5f401eb3aea26e3b04d9356646cb3ad973f
Brian Postlethwaite <brian.postlethwaite@health.telstra.com>
2020-03-05 14:53:46 +1100

    POC AzureStorage FHIR Server demo

---
+++ src/Hl7.DemoAzureStorageFhirServer.AspNetCore/Models/AzureBlobStorageResourceService.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.WebApi;
+using Hl7.Fhir.Utility;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.IO;
+using Hl7.Fhir.Serialization;
+using Newtonsoft.Json;
+
+namespace Hl7.DemoFileSystemFhirServer
+{
+    public class AzureBlobStorageResourceService : Hl7.Fhir.WebApi.IFhirResourceServiceR4<IServiceProvider>
+    {
+        public AzureBlobStorageResourceService()
+        {
+            if (_container == null)
+            {
+                // Retrieve storage account from connection string.
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+
+                // Create the blob client.
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                // Retrieve reference to a previously created container.
+                _containerName = "raw";
+                _container = blobClient.GetContainerReference(_containerName);
+                _container.CreateIfNotExistsAsync();
+            }
+        }
+
+        static CloudBlobContainer _container;
+        static string _containerName;
+        static FhirJsonSerializer serializer = new FhirJsonSerializer();
+
+        public ModelBaseInputs<IServiceProvider> RequestDetails { get; set; }
+
+        public string ResourceName { get; set; }
+
+        string FileName(string Id)
+        {
+            return $"{ResourceName}-{Id}.json";
+        }
+
+        public async Task<Resource> Create(Resource resource, string ifMatch, string ifNoneExist, DateTimeOffset? ifModifiedSince)
+        {
+            if (String.IsNullOrEmpty(resource.Id))
+                resource.Id = Guid.NewGuid().ToFhirId();
+            if (resource.Meta == null)
+                resource.Meta = new Meta();
+            resource.Meta.LastUpdated = DateTime.Now;
+            var dirRef = _container.GetDirectoryReference(_containerName);
+            var blob = dirRef.GetBlockBlobReference(FileName(resource.Id));
+            using (Stream stream = new MemoryStream())
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    using (JsonWriter jsonwriter = SerializationUtil.CreateJsonTextWriter(writer)) // This will use the BetterJsonWriter which handles precision correctly
+                    {
+                        serializer.Serialize(resource, jsonwriter);
+                        stream.Seek(0, SeekOrigin.Begin);
+                        await blob.UploadFromStreamAsync(stream);
+                        resource.SetAnnotation<CreateOrUpate>(CreateOrUpate.Create);
+                    }
+                }
+            }
+            return resource;
+        }
+
+        public async Task<string> Delete(string id, string ifMatch)
+        {
+            var dirRef = _container.GetDirectoryReference(_containerName);
+            var blob = dirRef.GetBlobReference(FileName(id));
+            if (await blob.ExistsAsync())
+                await blob.DeleteAsync();
+
+            return null;
+        }
+
+        public async Task<Resource> Get(string resourceId, string VersionId, SummaryType summary)
+        {
+            var dirRef = _container.GetDirectoryReference(_containerName);
+            var blob = dirRef.GetBlobReference(FileName(resourceId));
+            if (!await blob.ExistsAsync())
+                throw new FhirServerException(System.Net.HttpStatusCode.Gone, "It might have been deleted!");
+
+            var stream = await blob.OpenReadAsync();
+            var reader = SerializationUtil.JsonReaderFromStream(stream);
+            return new Fhir.Serialization.FhirJsonParser().Parse<Resource>(reader);
+        }
+
+        public Task<CapabilityStatement.ResourceComponent> GetRestResourceComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bundle> InstanceHistory(string ResourceId, DateTimeOffset? since, DateTimeOffset? Till, int? Count, SummaryType summary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Resource> PerformOperation(string operation, Parameters operationParameters, SummaryType summary)
+        {
+            if (operation == "count-em")
+            {
+                var result = new OperationOutcome();
+                result.Issue.Add(new OperationOutcome.IssueComponent()
+                {
+                    Code = OperationOutcome.IssueType.Informational,
+                    Severity = OperationOutcome.IssueSeverity.Information,
+                    Details = new CodeableConcept(null, null, $"{ResourceName} resource instances: {System.IO.Directory.EnumerateFiles(AzureBlobStorageSystemService.Directory, $"{ResourceName}.*.xml").Count()}")
+                });
+                return System.Threading.Tasks.Task.FromResult<Resource>(result);
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public Task<Resource> PerformOperation(string id, string operation, Parameters operationParameters, SummaryType summary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bundle> Search(IEnumerable<KeyValuePair<string, string>> parameters, int? Count, SummaryType summary)
+        {
+            throw new NotImplementedException();
+            //Bundle result = new Bundle();
+            //result.Meta = new Meta();
+            //result.Id = new Uri("urn:uuid:" + Guid.NewGuid().ToString("n")).OriginalString;
+            //result.Type = Bundle.BundleType.Searchset;
+            //result.ResourceBase = RequestDetails.BaseUri;
+
+            // Check that the Last Update value is correctly entered and that the count is at least as big as the data included
+            // and update the links
+            // result.ProcessLastModifiedFromEntriesAndLinks(Request.RequestUri, pagesize, pagenumber, snapshotID);
+        }
+
+        public Task<Bundle> TypeHistory(DateTimeOffset? since, DateTimeOffset? Till, int? Count, SummaryType summary)
+        {
+            Bundle result = new Bundle();
+            result.Meta = new Meta()
+            {
+                LastUpdated = DateTime.Now
+            };
+            result.Id = new Uri("urn:uuid:" + Guid.NewGuid().ToString("n")).OriginalString;
+            result.Type = Bundle.BundleType.History;
+
+            var parser = new Fhir.Serialization.FhirXmlParser();
+            var files = System.IO.Directory.EnumerateFiles(AzureBlobStorageSystemService.Directory, $"{ResourceName}.*.xml");
+            foreach (var filename in files)
+            {
+                var resource = parser.Parse<Resource>(System.IO.File.ReadAllText(filename));
+                result.AddResourceEntry(resource,
+                    ResourceIdentity.Build(RequestDetails.BaseUri,
+                        resource.ResourceType.ToString(),
+                        resource.Id,
+                        resource.Meta.VersionId).OriginalString);
+            }
+            result.Total = result.Entry.Count;
+
+            // also need to set the page links
+
+            return System.Threading.Tasks.Task.FromResult(result);
+        }
+    }
+}
---
+++ src/Hl7.DemoAzureStorageFhirServer.AspNetCore/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Hl7.Fhir.NetCoreApi;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+
+namespace Hl7.DemoFileSystemFhirServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            options.Level = System.IO.Compression.CompressionLevel.Fastest);
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                            {
+                               "application/fhir+xml",
+                               "application/fhir+json"
+                            });
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+
+            AzureBlobStorageSystemService.Directory = @"c:\temp\demoserver";
+            if (!System.IO.Directory.Exists(AzureBlobStorageSystemService.Directory))
+                System.IO.Directory.CreateDirectory(AzureBlobStorageSystemService.Directory);
+
+            var systemService = new AzureBlobStorageSystemService();
+
+            services.UseFhirServerController(systemService, options => { /* options.OutputFormatters.Add(new TelstraHealth.FhirHtmlFormatter.FhirHtmlOutputFormatter()); */ });
+            // services.AddScoped<TelstraHealth.FhirHtmlFormatter.RazorViewToStringRenderer>();
+
+            // register the Static Content
+            //services.Configure<RazorViewEngineOptions>(options =>
+            //{
+            //    options.FileProviders.Clear();
+            //    options.FileProviders.Add(new PhysicalFileProvider(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+            //});
+            services.AddRazorPages(options =>
+            {
+                options.RootDirectory = "/wwwroot";
+            });
+
+            // Tell the Net stack to only use TLS
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            // This should not be in any production system, it
+            // essentially permits dud certs being used
+            // unchecked.
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender2, cert2, chain, sslPolicyErrors) => true;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseResponseCompression();
+            // app.UseResponseBuffering();
+            app.UseStaticFiles(new StaticFileOptions(new Microsoft.AspNetCore.StaticFiles.Infrastructure.SharedOptions()
+            {
+                FileProvider = new PhysicalFileProvider(System.IO.Path.Combine(env.WebRootPath, "content")),
+                RequestPath = "/content"
+            }));
+
+            app.UseAuthentication();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapFallbackToFile(System.IO.Path.Combine(env.WebRootPath, "content"));
+            });
+        }
+    }
+}

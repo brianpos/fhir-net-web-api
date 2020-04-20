5db9b3231ddeea44d74e5fff5fe07a8bf0a03079
Brian Postlethwaite <brian.postlethwaite@health.telstra.com>
2020-04-20 15:05:58 +1000

    Fix the parsing of the Batch Transaction request call

--- src/Hl7.Fhir.WebApi.AspNetCore/FhirController.cs
+++ src/Hl7.Fhir.WebApi.AspNetCore/FhirController.cs
@@ -94,7 +94,7 @@ namespace Hl7.Fhir.WebApi
         /// http://hl7-fhir.github.io/http.html#transaction
         /// </summary>
         [HttpPost, Route("")]
-        public async Task<IActionResult> ProcessBatch(Bundle batch)
+        public async Task<IActionResult> ProcessBatch([FromBody]Bundle batch)
         {
             var buri = this.CalculateBaseURI("metadata");
             var Inputs = GetInputs(buri);

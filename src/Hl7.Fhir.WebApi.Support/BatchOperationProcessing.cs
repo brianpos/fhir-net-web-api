38f94f7596a5a68cd7a372a5ef2bca840a24e2f4
brian_pos <brian_pos@hotmail.com>
2021-08-03 23:35:15 +1000

    Move the batch support and utility functions into the support package

---
+++ src/Hl7.Fhir.WebApi.Support/BatchOperationProcessing.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Utility;
+
+namespace Hl7.Fhir.WebApi
+{
+    public class BatchOperationProcessing<TSP>
+        where TSP: class
+    {
+        public Func<ModelBaseInputs<TSP>, string, IFhirResourceServiceR4<TSP>> GetResourceService { get; set; }
+        public int DefaultPageSize { get; set; } = 40;
+
+        public async System.Threading.Tasks.Task<Bundle> ProcessBatch(ModelBaseInputs<TSP> request, Bundle batch)
+        {
+            Bundle outcome = new Bundle();
+            outcome.Type = Bundle.BundleType.TransactionResponse;
+            outcome.Entry = new List<Bundle.EntryComponent>();
+            outcome.ResourceBase = request.BaseUri;
+            if (batch.Entry != null)
+            {
+                // mapping from batch identities to local resource identities (in our DB)
+                Dictionary<string, string> mappedResourceIds = new Dictionary<string, string>();
+
+                // http://hl7.org/fhir/http.html#transaction
+                // Perform the validation of the batch first
+
+                // DELETE
+                foreach (var entry in batch.Entry.Where(e => e.Request != null && e.Request.Method == Bundle.HTTPVerb.DELETE))
+                {
+                    await ProcessBatchEntry(request, outcome, mappedResourceIds, entry);
+                }
+                // POST
+                foreach (var entry in batch.Entry.Where(e => e.Request != null && e.Request.Method == Bundle.HTTPVerb.POST))
+                {
+                    await ProcessBatchEntry(request, outcome, mappedResourceIds, entry);
+                }
+                // PUT
+                foreach (var entry in batch.Entry.Where(e => e.Request != null && e.Request.Method == Bundle.HTTPVerb.PUT))
+                {
+                    await ProcessBatchEntry(request, outcome, mappedResourceIds, entry);
+                }
+                // GET
+                foreach (var entry in batch.Entry.Where(e => e.Request != null && e.Request.Method == Bundle.HTTPVerb.GET))
+                {
+                    await ProcessBatchEntry(request, outcome, mappedResourceIds, entry);
+                }
+
+                // Request-less bundle handling
+                foreach (var entry in batch.Entry.Where(e => e.Request == null))
+                {
+                    await ProcessBatchEntry(request, outcome, mappedResourceIds, entry);
+                }
+
+            }
+            outcome.Total = outcome.Entry.Count;
+            return outcome;
+        }
+
+        private async System.Threading.Tasks.Task ProcessBatchEntry(ModelBaseInputs<TSP> request, Bundle outcome, Dictionary<string, string> mappedResourceIds, Bundle.EntryComponent entry)
+        {
+            if (entry.Request == null)
+            {
+                entry.Request = new Bundle.RequestComponent();
+                if (!string.IsNullOrEmpty(entry.Resource.Id))
+                {
+                    entry.Request.Method = Bundle.HTTPVerb.PUT;
+                    entry.Request.Url = entry.Resource.ResourceType + "/" + entry.Resource.Id;
+                }
+                else
+                    entry.Request.Method = Bundle.HTTPVerb.POST;
+            }
+            if (entry.Request != null)
+            {
+                Bundle.EntryComponent itemResult = new Bundle.EntryComponent();
+                itemResult.FullUrl = entry.FullUrl;
+                itemResult.Request = entry.Request;
+                outcome.Entry.Add(itemResult);
+                string resourceType = null;
+                if (entry.Resource != null)
+                {
+                    resourceType = entry.Resource.ResourceType.ToString();
+                }
+                else if (!string.IsNullOrEmpty(entry.Request.Url))
+                {
+                    string url = entry.Request.Url;
+                    if (url.Contains("?"))
+                    {
+                        resourceType = url.Substring(0, url.IndexOf("?"));
+                        if (resourceType.Contains("/"))
+                            resourceType = resourceType.Substring(resourceType.IndexOf("/") + 1);
+                        FHIRAllTypes rt;
+                        if (!Enum.TryParse<FHIRAllTypes>(resourceType, out rt))
+                            resourceType = null;
+                    }
+                    else
+                    {
+                        ResourceIdentity ri = new ResourceIdentity(url);
+                        resourceType = ri.ResourceType;
+                    }
+                }
+                if (string.IsNullOrEmpty(resourceType))
+                {
+                    itemResult.Response = new Bundle.ResponseComponent();
+                    itemResult.Response.Status = HttpStatusCode.BadRequest.ToString();
+                    OperationOutcome outcomeItem = new OperationOutcome();
+                    outcomeItem.Issue.Add(new OperationOutcome.IssueComponent()
+                    {
+                        Severity = OperationOutcome.IssueSeverity.Error,
+                        Details = new CodeableConcept(null, null, "Resource Type [" + resourceType + "] was not found")
+                    });
+                    itemResult.Resource = outcomeItem;
+                    return;
+                }
+                System.Diagnostics.Trace.WriteLine(String.Format("Transaction ({1}) url: {0}", entry.Request.Url, resourceType));
+                var model = GetResourceService(request, resourceType);
+                if (model != null)
+                {
+                    if (entry.Request.Method == Bundle.HTTPVerb.PUT || entry.Request.Method == Bundle.HTTPVerb.POST)
+                    {
+                        itemResult.Response = new Bundle.ResponseComponent();
+                        // itemResult.TransactionResponse.TypeName = entry.Transaction.TypeName;
+                        try
+                        {
+                            string oldId = resourceType + "/" + entry.Resource.Id;
+                            if (entry.Request.Method == Bundle.HTTPVerb.POST)
+                                entry.Resource.Id = null;
+
+                            // Replace any of the internal reference IDs with ones
+                            // that have been remapped
+                            var ids = entry.Resource.AllReferences();
+                            foreach (var resRef in ids.Where(rr => !String.IsNullOrEmpty(rr.Reference)))
+                            {
+                                // TODO: If this is an identifier reference, then we must resolve the reference
+
+                                // Otherwise these are just normal references
+                                if (!string.IsNullOrEmpty(resRef.Reference))
+                                {
+                                    ResourceIdentity riEntity = new ResourceIdentity(resRef.Reference);
+                                    if (mappedResourceIds.ContainsKey(resRef.Reference))
+                                    {
+                                        resRef.Reference = mappedResourceIds[resRef.Reference];
+                                    }
+                                }
+                            }
+
+                            // Store the changes
+                            Resource r = await model.Create(entry.Resource, entry.Request?.IfMatch, entry.Request?.IfNoneExist, entry.Request?.IfModifiedSince);
+                            if (!string.IsNullOrEmpty(oldId) && oldId != resourceType + "/" && !mappedResourceIds.ContainsKey(oldId))
+                                mappedResourceIds.Add(oldId, resourceType + "/" + r.Id);
+
+                            itemResult.Resource = r;
+                            if (r.Annotation<CreateOrUpate>() == CreateOrUpate.Create)
+                                itemResult.Response.Status = ((int)HttpStatusCode.Created).ToString();
+                            else
+                                itemResult.Response.Status = ((int)HttpStatusCode.OK).ToString();
+
+                            // Also include the Location and ETag values here
+                            itemResult.Response.Location = r.ResourceIdentity().OriginalString;
+                            itemResult.Response.Etag = String.Format("W/\"{0}\"", r.Meta.VersionId);
+                        }
+                        catch (FhirServerException ex)
+                        {
+                            itemResult.Response.Status = ((int)ex.StatusCode).ToString();
+                            itemResult.Resource = ex.Outcome;
+                        }
+                        catch (FhirOperationException ex)
+                        {
+                            itemResult.Response.Status = ((int)ex.Status).ToString();
+                            itemResult.Resource = ex.Outcome;
+                        }
+                    }
+                    else if (entry.Request.Method == Bundle.HTTPVerb.DELETE)
+                    {
+                        itemResult.Response = new Bundle.ResponseComponent();
+                        try
+                        {
+                            ResourceIdentity ri = new ResourceIdentity(entry.Request.Url);
+                            if (mappedResourceIds.ContainsKey(resourceType + "/" + ri.Id))
+                            {
+                                // This is an error transaction
+                            }
+                            string deletedIdentity = await model.Delete(ri.Id, entry.Request.IfMatch);
+
+                            itemResult.Response.Status = ((int)HttpStatusCode.NoContent).ToString();
+
+                            // Also include the Location and ETag values here
+                            if (!string.IsNullOrEmpty(deletedIdentity))
+                            {
+                                ResourceIdentity riDelete = new ResourceIdentity(deletedIdentity);
+                                itemResult.Response.Etag = String.Format("W/\"{0}\"", riDelete.VersionId);
+                            }
+                        }
+                        catch (FhirOperationException ex)
+                        {
+                            itemResult.Response.Status = ex.Status.ToString();
+                            itemResult.Resource = ex.Outcome;
+                        }
+                        catch (FhirServerException ex)
+                        {
+                            itemResult.Response.Status = ((int)ex.StatusCode).ToString();
+                            itemResult.Resource = ex.Outcome;
+                        }
+                    }
+                    else
+                    {
+                        // implement the GET searching
+                        itemResult.Response = new Bundle.ResponseComponent();
+                        var query = new Uri(entry.Request.Url, UriKind.RelativeOrAbsolute);
+                        if (query.IsAbsoluteUri)
+                        {
+                            itemResult.Response.Status = HttpStatusCode.MethodNotAllowed.ToString(); // "Not Supported: " + entry.Transaction.Url;
+                        }
+                        else if (entry.Request.Url.StartsWith($"{resourceType}?"))
+                        {
+                            try
+                            {
+                                var parameters = System.Web.HttpUtility.ParseQueryString(new Uri(request.BaseUri + entry.Request.Url).Query).TupledParameters(false);
+                                int pagesize;
+                                string pageSizeString = parameters.Where(k => k.Key == "_count").FirstOrDefault().Value;
+                                if (string.IsNullOrEmpty(pageSizeString) || !int.TryParse(pageSizeString, out pagesize))
+                                    pagesize = DefaultPageSize;
+                                Bundle result = await model.Search(parameters, pagesize, SummaryType.False);
+                                // result.Id = new Uri("urn:uuid:" + Guid.NewGuid().ToString("n")).OriginalString;
+                                itemResult.Resource = result;
+                                itemResult.Response.Status = ((int)HttpStatusCode.OK).ToString();
+                            }
+                            catch (FhirOperationException ex)
+                            {
+                                itemResult.Response.Status = ex.Status.ToString();
+                                itemResult.Resource = ex.Outcome;
+                            }
+                            catch (FhirServerException ex)
+                            {
+                                itemResult.Response.Status = ((int)ex.StatusCode).ToString();
+                                itemResult.Resource = ex.Outcome;
+                            }
+                        }
+                        else
+                        {
+                            // this is a regular get, so check it for resource Identity
+                            ResourceIdentity ri = new ResourceIdentity(query);
+                            itemResult.Resource = await model.Get(ri.Id, ri.VersionId, SummaryType.False);
+
+                            itemResult.Response.Status = ((int)HttpStatusCode.OK).ToString();
+
+                            // Also include the Location and ETag values here
+                            itemResult.Response.Location = itemResult.Resource.ResourceIdentity().OriginalString;
+                            itemResult.Response.Etag = String.Format("W/\"{0}\"", itemResult.Resource.Meta.VersionId);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Hl7.Fhir.WebApi.Support/ResourceExtensions.cs
@@ -0,0 +1,152 @@
+﻿/* 
+ * Copyright (c) 2017+ brianpos, Firely and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://github.com/ewoutkramer/fhir-net-api/blob/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hl7.Fhir.WebApi
+{
+    public static class ResourceExtensions
+    {
+        #region << Parameter Extractions Extensions >>
+        public static string GetString(this Parameters me, string name)
+        {
+            var value = me.Parameter.Where(s => s.Name == name).FirstOrDefault();
+            if (value == null)
+                return null;
+            if (value.Value as FhirString != null)
+                return ((FhirString)value.Value).Value;
+            if (value.Value as FhirUri != null)
+                return ((FhirUri)value.Value).Value;
+            return null;
+        }
+
+        public static Resource GetResource(this Parameters me, string name)
+        {
+            var value = me.Parameter.Where(s => s.Name == name).FirstOrDefault();
+            if (value == null)
+                return null;
+            return value.Resource;
+        }
+        #endregion
+
+        public static Uri AppendToQuery(this Uri me, string query)
+        {
+            if (string.IsNullOrEmpty(me.Query))
+                return new Uri(me.OriginalString + "?" + query.TrimStart('?').TrimEnd('&'));
+            return new Uri(me.OriginalString + "&" + query.TrimEnd('&'));
+        }
+
+        #region << Resource Extension Methods >>
+        public static List<String> ReservedSearchParams = new List<string>() { "_id", "_language", "_lastUpdated", "_profile", "_security", "_tag", "_has" };
+
+        public static IEnumerable<KeyValuePair<string, string>> TupledParameters(this System.Collections.Specialized.NameValueCollection query, bool filterReservedParameters)
+        {
+            var list = new List<KeyValuePair<string, string>>();
+
+            if (query.HasKeys())
+            {
+                foreach (string key in query.Keys)
+                {
+                    if (!filterReservedParameters || !key.StartsWith("_") || ReservedSearchParams.Contains(key))
+                        list.Add(new KeyValuePair<string, string>(key, query[key]));
+                }
+            }
+            return list;
+        }
+
+        public static List<ResourceReference> AllReferences(this Base resource)
+        {
+            List<ResourceReference> results = new List<ResourceReference>();
+            if (resource == null)
+                return results;
+
+            if (resource is IExtendable extendable)
+            {
+                foreach (var i in extendable.Extension)
+                {
+                    if (i.Value is ResourceReference resRef)
+                        results.Add(resRef);
+                }
+            }
+            if (resource is IModifierExtendable modExtendable)
+            {
+                foreach (var i in modExtendable.ModifierExtension)
+                {
+                    if (i.Value is ResourceReference resRef)
+                        results.Add(resRef);
+                }
+            }
+
+            var mapping = BaseFhirParser.Inspector.ImportType(resource.GetType());
+            var propMappings = mapping.PropertyMappings.Where(t => t.ElementType.Name == "ResourceReference" || t.ElementType.Name == "Resource" || t.ElementType.BaseType.Name == "BackboneElement" || t.Choice == Hl7.Fhir.Introspection.ChoiceType.DatatypeChoice);
+            foreach (var item in propMappings)
+            {
+                if (item.ElementType.BaseType.Name == "BackboneElement")
+                {
+                    if (item.IsCollection)
+                    {
+                        System.Collections.IEnumerable col = item.GetValue(resource) as System.Collections.IEnumerable;
+                        foreach (var e in col)
+                        {
+                            BackboneElement be = e as BackboneElement;
+                            results.AddRange(be.AllReferences());
+                        }
+                    }
+                    else
+                    {
+                        BackboneElement be = item.GetValue(resource) as BackboneElement;
+                        results.AddRange(be.AllReferences());
+                    }
+                }
+                else if (item.ElementType.Name == "Resource")
+                {
+                    if (item.IsCollection)
+                    {
+                        System.Collections.IEnumerable col = item.GetValue(resource) as System.Collections.IEnumerable;
+                        foreach (var e in col)
+                        {
+                            Resource r = e as Resource;
+                            results.AddRange(r.AllReferences());
+                        }
+                    }
+                    else
+                    {
+                        Resource r = item.GetValue(resource) as Resource;
+                        results.AddRange(r.AllReferences());
+                    }
+                }
+                else
+                {
+                    if (item.IsCollection)
+                    {
+                        System.Collections.IEnumerable col = item.GetValue(resource) as System.Collections.IEnumerable;
+                        foreach (var e in col)
+                        {
+                            ResourceReference rr = e as ResourceReference;
+                            if (rr != null)
+                                results.Add(rr);
+                        }
+                    }
+                    else
+                    {
+                        ResourceReference rr = item.GetValue(resource) as ResourceReference;
+                        if (rr != null)
+                            results.Add(rr);
+                    }
+                }
+            }
+            return results;
+        }
+        #endregion
+    }
+}

880309d0d25fcf655c637f218674880cb25c155a
Brian Postlethwaite <brian_pos@hotmail.com>
2024-05-15 19:49:34 +1000

    Update the function app sample

--- src/Hl7.DemoSDCExtractOperation/Program.cs
+++ src/Hl7.DemoSDCExtractOperation/Program.cs
@@ -12,35 +12,31 @@ using Hl7.Fhir.Rest;
 
 namespace Hl7.DemoFhirAzureFunctionApp
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults((IFunctionsWorkerApplicationBuilder builder) =>
-                {
-                    var config = new ConfigurationBuilder()
-                        .AddEnvironmentVariables()
-                        .Build();
-                    builder.Services.AddTransient<FhirClient>((services) => { return new FhirClient(config["baseURL"]); });
+	public class Program
+	{
+		public static void Main()
+		{
+			var config = new ConfigurationBuilder()
+				.AddEnvironmentVariables()
+				.Build();
 
-                    // Register our custom middlewares with the worker
-                    builder.UseMiddleware<ExceptionHandlingMiddleware>();
+			var builder =
+			new HostBuilder()
+				.ConfigureFunctionsWebApplication((context, builder2) =>
+				{
+					builder2.Services.AddTransient<FhirClient>((services) => { return new FhirClient(config["BaseURL"]); });
+					// Register our custom middlewares with the worker
+					builder2.UseMiddleware<ExceptionHandlingMiddleware>();
 
-                    builder.UseWhen<StampHttpHeaderMiddleware>((context) =>
-                    {
-                        // We want to use this middleware only for http trigger invocations.
-                        return context.FunctionDefinition.InputBindings.Values
-                                      .First(a => a.Type.EndsWith("Trigger")).Type == "httpTrigger";
-                    });
-                },
-                (WorkerOptions options) => {
-                    // Register a converter that will handle converting a Resource derived class as a model binding from the http request body
-                    // It will work out if the content is XML or JSON and convert that on the way in too.
-                    options.InputConverters.Register<FhirInputConverter>();
-                })
-                .Build();
-            host.Run();
-        }
-    }
+					builder2.UseWhen<StampHttpHeaderMiddleware>((context) =>
+					{
+						// We want to use this middleware only for http trigger invocations.
+						return context.FunctionDefinition.InputBindings.Values
+									  .First(a => a.Type.EndsWith("Trigger")).Type == "httpTrigger";
+					});
+				});
+			var host = builder.Build();
+			host.Run();
+		}
+	}
 }
